034fbfb519037fdf11f64452843619523c4c7218
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-09-17 08:53:08 +0200

    updated test

--- tests/configurations/proxy_conf.py
+++ tests/configurations/proxy_conf.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # pylint: disable = missing-docstring
 # -*- coding: utf-8 -*-
-from saml2 import BINDING_HTTP_REDIRECT
-from saml2 import BINDING_HTTP_POST
 import os.path
-from vopaas_proxy.util.attribute_module import IdentityAttributes
 
 # try:
 #     from saml2.sigver import get_xmlsec_binary
@@ -23,31 +20,6 @@ PORT = 8090
 
 BASE = 'https://%s:%s' % (HOST, PORT)
 
-# Discovery to server if proxy is used with multiple IdP's
-DISCO_SRV = "http://localhost/role/idp.ds"
-
-ATTRIBUTE_MODULE = IdentityAttributes()
-
 PLUGIN_PATH = [full_path(".")]
 BACKEND_MODULES = ["saml2_module_conf"]
-
-ENDPOINTS = {"single_sign_on_service": {BINDING_HTTP_REDIRECT: "sso/redirect",
-                                        BINDING_HTTP_POST: "sso/post"}}
-
-CONFIG = {
-    "backends": {},
-    "entityid": "{}/proxy.xml".format(BASE),
-    "service": {
-        "idp": {
-            "endpoints": {
-                "single_sign_on_service": [],
-            },
-        },
-    },
-    "key_file": full_path("../pki/key.pem"),
-    "cert_file": full_path("../pki/cert.pem"),
-    "metadata": {
-        "local": [full_path("unittest_sp.xml")],
-    },
-    "xmlsec_binary": xmlsec_path,
-}
+FRONTEND_MODULES = ["saml2_frontend_plugin"]
---
+++ tests/configurations/saml2_frontend_plugin.py
@@ -0,0 +1,44 @@
+import os
+from saml2 import BINDING_HTTP_REDIRECT, BINDING_HTTP_POST
+from vopaas_proxy.plugin_base.frontend import FrontendPlugin
+from vopaas_proxy.frontends.saml2_idp import SamlIDP
+
+__author__ = 'mathiashedstrom'
+
+
+def full_path(local_file):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(basedir, local_file)
+
+
+XMLSEC_PATH = '/usr/local/bin/xmlsec1'
+
+MODULE = SamlIDP
+RECEIVER = "VOpaasIDP"
+ENDPOINTS = {"single_sign_on_service": {BINDING_HTTP_REDIRECT: "sso/redirect",
+                                        BINDING_HTTP_POST: "sso/post"}}
+
+
+def setup(base):
+    idpConfig = {
+        "entityid": "{}/proxy.xml".format(base),
+        "service": {
+            "idp": {
+                "endpoints": {
+                    "single_sign_on_service": [],
+                },
+            },
+        },
+        "key_file": full_path("../pki/key.pem"),
+        "cert_file": full_path("../pki/cert.pem"),
+        "metadata": {
+            "local": [full_path("unittest_sp.xml")],
+        },
+        "xmlsec_binary": XMLSEC_PATH,
+    }
+
+    config = {"idp_config": idpConfig,
+              "endpoints": ENDPOINTS,
+              "base": base}
+
+    return FrontendPlugin(MODULE, RECEIVER, config)
