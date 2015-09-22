e6e9f71d8385f352459a85a39ff59315213aefd5
Hans Hörberg <hans.horberg@umu.se>
2015-09-22 10:30:08 +0200

    Changed all values to lists, to make the test verification easier.

--- tests/test_users.py
+++ tests/test_users.py
@@ -5,21 +5,21 @@ __author__ = 'haho0032'
 
 USERS = {
     "testuser1": {
-        "sn": "Testsson 1",
-        "givenName": "Test 1",
-        "eduPersonAffiliation": "student",
-        "eduPersonScopedAffiliation": "student@example.com",
-        "eduPersonPrincipalName": "test@example.com",
-        "uid": "testuser1",
-        "eduPersonTargetedID": "one!for!all",
-        "c": "SE",
-        "o": "Example Co.",
-        "ou": "IT",
-        "initials": "P",
-        "schacHomeOrganization": "example.com",
-        "email": "hans@example.com",
-        "displayName": "Test Testsson",
-        "labeledURL": "http://www.example.com/haho My homepage",
-        "norEduPersonNIN": "SE199012315555"
+        "sn": ["Testsson 1"],
+        "givenName": ["Test 1"],
+        "eduPersonAffiliation": ["student"],
+        "eduPersonScopedAffiliation": ["student@example.com"],
+        "eduPersonPrincipalName": ["test@example.com"],
+        "uid": ["testuser1"],
+        "eduPersonTargetedID": ["one!for!all"],
+        "c": ["SE"],
+        "o": ["Example Co."],
+        "ou": ["IT"],
+        "initials": ["P"],
+        "schacHomeOrganization": ["example.com"],
+        "email": ["test@example.com"],
+        "displayName": ["Test Testsson"],
+        "labeledURL": ["http://www.example.com/haho My homepage"],
+        "norEduPersonNIN": ["SE199012315555"]
     }
 }
