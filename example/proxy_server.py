5942161f774246e5730c422154ecc22381d2c673
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-09-24 16:24:53 +0200

    Removed wsgi_server from satosa. Added wsgiApplication to example proxy_server

--- example/proxy_server.py
+++ example/proxy_server.py
@@ -1,13 +1,66 @@
 import argparse
 import os
+import logging
 import sys
+import traceback
 
 import cherrypy
 from beaker.middleware import SessionMiddleware
 from werkzeug.debug import DebuggedApplication
-from satosa.satosa_config import SATOSAConfig
+from saml2.httputil import Unauthorized
+from saml2.httputil import NotFound
 
-from satosa.wsgi_server import WsgiApplication
+from saml2.httputil import ServiceError
+
+from satosa.satosa_config import SATOSAConfig
+from satosa.base import SATOSABase
+from satosa.request_context import RequestContext
+from satosa.routing import NoBoundEndpointError
+from satosa.service import unpack_either
+
+LOGGER = logging.getLogger("")
+LOGFILE_NAME = 's2s.log'
+hdlr = logging.FileHandler(LOGFILE_NAME)
+base_formatter = logging.Formatter("%(asctime)s %(name)s:%(levelname)s %(message)s")
+
+hdlr.setFormatter(base_formatter)
+LOGGER.addHandler(hdlr)
+LOGGER.setLevel(logging.DEBUG)
+
+
+class WsgiApplication(object):
+    def __init__(self, config, debug=False):
+        self.debug = debug
+        if config is None:
+            raise ValueError("Missing configuration")
+        self.satosa = SATOSABase(config)
+
+    def run_server(self, environ, start_response):
+        path = environ.get('PATH_INFO', '').lstrip('/')
+        if ".." in path:
+            resp = Unauthorized()
+            return resp(environ, start_response)
+
+        context = RequestContext()
+        context.path = path
+        context.request = unpack_either(environ)
+
+        try:
+            resp = self.satosa.run(context)
+            return resp(environ, start_response)
+        except NoBoundEndpointError:
+            LOGGER.debug("unknown side: %s" % path)
+            resp = NotFound("Couldn't find the side you asked for!")
+            return resp(environ, start_response)
+        except Exception as err:
+            if not self.debug:
+                print("%s" % err, file=sys.stderr)
+                traceback.print_exc()
+                LOGGER.exception("%s" % err)
+                resp = ServiceError("%s" % err)
+                return resp(environ, start_response)
+            else:
+                raise
 
 
 def main():
