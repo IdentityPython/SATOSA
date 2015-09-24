d9f3249e380258342a54977dd0af0a650f05af88
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-09-24 16:54:19 +0200

    Added comments to routing

--- src/satosa/routing.py
+++ src/satosa/routing.py
@@ -1,3 +1,6 @@
+"""
+Holds satosa routing logic
+"""
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 import json
 import re
@@ -7,18 +10,31 @@ __author__ = 'mathiashedstrom'
 
 
 class NoBoundEndpointError(Exception):
+    """
+    Raised when a given url path is not bound to any endpoint function
+    """
     pass
 
 
 class UnknownTargetBackend(Exception):
+    """
+    Raised when targeting an unknown backend
+    """
     pass
 
 
 class ModuleRouter():
+    """
+    Routes url paths to their bound functions
+    and handles the internal routing between frontends and backends.
+    """
     def __init__(self, frontends, backends):
         """
-        :param frontends: All available frontends
-        :param backends: All available backends
+        :type frontends: dict[str, satosa.frontends.base.FrontendModule]
+        :type backends: dict[str, satosa.backends.base.BackendModule]
+
+        :param frontends: All available frontends used by the proxy. Key as frontend name, value as module
+        :param backends: All available backends used by the proxy. Key as backend name, value as module
         """
 
         if not (frontends and backends):
@@ -40,8 +56,13 @@ class ModuleRouter():
     def backend_routing(self, context, state):
         """
         Returns the targeted backend and an updated state
+
+        :type context: satosa.request_context.RequestContext
+        :type state: str
+        :rtype (satosa.backends.base.BackendModule, str)
+
         :param context: The request context
-        :param state: The state of a frontend
+        :param state: The current state
         :return: (backend, state)
         """
 
@@ -53,6 +74,10 @@ class ModuleRouter():
     def frontend_routing(self, state):
         """
         Returns the targeted frontend and original state
+
+        :type state: str
+        :rtype (satosa.frontends.base.FrontendModule, str)
+
         :param state: The state created in the incoming function
         :return: (frontend, state)
         """
@@ -63,6 +88,16 @@ class ModuleRouter():
         return frontend, request_state
 
     def _validate_context(self, context):
+        """
+        Validates values in the context needed by the ModuleRouter.
+        Raises BadContextError if any error.
+
+        :type context: satosa.request_context.RequestContext
+        :rtype: None
+
+        :param context: The request context
+        :return: None
+        """
         if not context:
             raise BadContextError("Context is None")
         if context.path is None:
@@ -71,9 +106,12 @@ class ModuleRouter():
     def endpoint_routing(self, context):
         """
         Finds and returns the endpoint function bound to the path
+
+        :type context: satosa.request_context.RequestContext
+        :rtype: (function, *args)
+
         :param context: The request context
-        :param path: url path
-        :return: registered endpoint
+        :return: registered endpoint and bound parameters
         """
         self._validate_context(context)
 
