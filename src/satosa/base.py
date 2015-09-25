f22ec1215bce5d29443766ff06f2b6f5498c1164
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-09-25 14:26:30 +0200

    Added some comments to SATOSABase

--- src/satosa/base.py
+++ src/satosa/base.py
@@ -14,13 +14,15 @@ class SATOSABase():
 
     def _auth_req_callback_func(self, context, internal_request, state):
         """
-        An Authentication request has been requested, this is the second step
-        in the sequence
+        This function is called by a frontend module when an authorization request has been processed.
 
-        :param info: Information about the authentication request
-        :param environ: WSGI environment
-        :param start_response: WSGI start_response
-        :param state_key:
+        :type context: satosa.request_context.RequestContext
+        :type internal_request: dict
+        :type state: str
+
+        :param context: The request context
+        :param internal_request: request processed by the frontend
+        :param state: the current state
 
         :return: response
         """
@@ -29,22 +31,44 @@ class SATOSABase():
 
     def _auth_resp_callback_func(self, context, internal_response, state):
         """
-        An authentication response has been received and now an authentication
-        response from this server should be constructed.
+        This function is called by a backend module when the authorization is complete.
 
-        :param internal_response: The Authentication response
-        :param instance: SP instance that received the authentication response
+        :type context: satosa.request_context.RequestContext
+        :type internal_response: dict
+        :type state: str
+
+        :param context: The request context
+        :param internal_response: The authentication response
+        :param state: The current state
         :return: response
         """
+
         frontend, state = self.module_router.frontend_routing(state)
         return frontend.handle_authn_response(context, internal_response, state)
 
     def _run_bound_endpoint(self, context, spec):
+        """
+
+        :type context: satosa.request_context.RequestContext
+        :type spec: (satosa.request_context.RequestContext) -> object
+
+        :param context: The request context
+        :param spec: bound endpoint function
+        :return: response
+        """
         if isinstance(spec, tuple):
             return spec[0](context, *spec[1:])
         else:
             return spec()
 
     def run(self, context):
+        """
+        Runs the satosa proxy with the given context.
+
+        :type context: satosa.request_context.RequestContext
+
+        :param context: The request context
+        :return: response
+        """
         spec = self.module_router.endpoint_routing(context)
         return self._run_bound_endpoint(context, spec)
