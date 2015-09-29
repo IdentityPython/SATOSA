0525c3bf311054c9c4d3b3f2add9ad09dfb3ae41
Hans Hörberg <hans.horberg@umu.se>
2015-09-29 16:49:21 +0200

    Internal data representation.

---
+++ src/satosa/internal_data.py
@@ -0,0 +1,456 @@
+"""
+The module contains internal data representation in SATOSA and general converteras that can be used
+for converting from SAML/OAuth/OpenID connect to the internal representation.
+"""
+from enum import Enum
+
+__author__ = 'haho0032'
+
+SATOSA_ATTRIBUTES = {
+    'aRecord': True,
+    'aliasedEntryName': True,
+    'aliasedObjectName': True,
+    'associatedDomain': True,
+    'associatedName': True,
+    'audio': True,
+    'authorityRevocationList': True,
+    'buildingName': True,
+    'businessCategory': True,
+    'c': True,
+    'cACertificate': True,
+    'cNAMERecord': True,
+    'carLicense': True,
+    'certificateRevocationList': True,
+    'cn': True,
+    'co': True,
+    'commonName': True,
+    'countryName': True,
+    'crossCertificatePair': True,
+    'dITRedirect': True,
+    'dSAQuality': True,
+    'dc': True,
+    'deltaRevocationList': True,
+    'departmentNumber': True,
+    'description': True,
+    'destinationIndicator': True,
+    'displayName': True,
+    'distinguishedName': True,
+    'dmdName': True,
+    'dnQualifier': True,
+    'documentAuthor': True,
+    'documentIdentifier': True,
+    'documentLocation': True,
+    'documentPublisher': True,
+    'documentTitle': True,
+    'documentVersion': True,
+    'domainComponent': True,
+    'drink': True,
+    'eduOrgHomePageURI': True,
+    'eduOrgIdentityAuthNPolicyURI': True,
+    'eduOrgLegalName': True,
+    'eduOrgSuperiorURI': True,
+    'eduOrgWhitePagesURI': True,
+    'eduPersonAffiliation': True,
+    'eduPersonEntitlement': True,
+    'eduPersonNickname': True,
+    'eduPersonOrgDN': True,
+    'eduPersonOrgUnitDN': True,
+    'eduPersonPrimaryAffiliation': True,
+    'eduPersonPrimaryOrgUnitDN': True,
+    'eduPersonPrincipalName': True,
+    'eduPersonScopedAffiliation': True,
+    'eduPersonTargetedID': True,
+    'email': True,
+    'emailAddress': True,
+    'employeeNumber': True,
+    'employeeType': True,
+    'enhancedSearchGuide': True,
+    'facsimileTelephoneNumber': True,
+    'favouriteDrink': True,
+    'fax': True,
+    'federationFeideSchemaVersion': True,
+    'friendlyCountryName': True,
+    'generationQualifier': True,
+    'givenName': True,
+    'gn': True,
+    'homePhone': True,
+    'homePostalAddress': True,
+    'homeTelephoneNumber': True,
+    'host': True,
+    'houseIdentifier': True,
+    'info': True,
+    'initials': True,
+    'internationaliSDNNumber': True,
+    'janetMailbox': True,
+    'jpegPhoto': True,
+    'knowledgeInformation': True,
+    'l': True,
+    'labeledURI': True,
+    'localityName': True,
+    'mDRecord': True,
+    'mXRecord': True,
+    'mail': True,
+    'mailPreferenceOption': True,
+    'manager': True,
+    'member': True,
+    'mobile': True,
+    'mobileTelephoneNumber': True,
+    'nSRecord': True,
+    'name': True,
+    'norEduOrgAcronym': True,
+    'norEduOrgNIN': True,
+    'norEduOrgSchemaVersion': True,
+    'norEduOrgUniqueIdentifier': True,
+    'norEduOrgUniqueNumber': True,
+    'norEduOrgUnitUniqueIdentifier': True,
+    'norEduOrgUnitUniqueNumber': True,
+    'norEduPersonBirthDate': True,
+    'norEduPersonLIN': True,
+    'norEduPersonNIN': True,
+    'o': True,
+    'objectClass': True,
+    'organizationName': True,
+    'organizationalStatus': True,
+    'organizationalUnitName': True,
+    'otherMailbox': True,
+    'ou': True,
+    'owner': True,
+    'pager': True,
+    'pagerTelephoneNumber': True,
+    'personalSignature': True,
+    'personalTitle': True,
+    'photo': True,
+    'physicalDeliveryOfficeName': True,
+    'pkcs9email': True,
+    'postOfficeBox': True,
+    'postalAddress': True,
+    'postalCode': True,
+    'preferredDeliveryMethod': True,
+    'preferredLanguage': True,
+    'presentationAddress': True,
+    'protocolInformation': True,
+    'pseudonym': True,
+    'registeredAddress': True,
+    'rfc822Mailbox': True,
+    'roleOccupant': True,
+    'roomNumber': True,
+    'sOARecord': True,
+    'searchGuide': True,
+    'secretary': True,
+    'seeAlso': True,
+    'serialNumber': True,
+    'singleLevelQuality': True,
+    'sn': True,
+    'st': True,
+    'stateOrProvinceName': True,
+    'street': True,
+    'streetAddress': True,
+    'subtreeMaximumQuality': True,
+    'subtreeMinimumQuality': True,
+    'supportedAlgorithms': True,
+    'supportedApplicationContext': True,
+    'surname': True,
+    'telephoneNumber': True,
+    'teletexTerminalIdentifier': True,
+    'telexNumber': True,
+    'textEncodedORAddress': True,
+    'title': True,
+    'uid': True,
+    'uniqueIdentifier': True,
+    'uniqueMember': True,
+    'userCertificate': True,
+    'userClass': True,
+    'userPKCS12': True,
+    'userPassword': True,
+    'userSMIMECertificate': True,
+    'userid': True,
+    'x121Address': True,
+    'x500UniqueIdentifier': True
+}
+
+OIDC_TO_SATOSA = {
+    'sub': 'eduPersonTargetedID',
+    'name': 'name',
+    'given_name': 'givenName',
+    'family_name': 'surname',
+    'middle_name': None,
+    'nickname': 'eduPersonNickname',
+    'preferred_username': 'userid',
+    'profile_string': '',           #URL of the End-User's profile page.
+    'picture': None,	            #URL of the End-User's profile picture.
+    'website': None,                #URL of the End-User's Web page or blog.
+    'email': 'email',
+    'email_verified': None,	        #boolean - True if the End-User's e-mail address is verified;
+    'gender': None,                 #End-User's gender.
+    'birthdate': None,              #End-User's birthday
+    'zoneinfo': None,               #time zone database representing the End-User's time zone.
+    'locale': None,                 #End-User's locale
+    'phone_number': None,
+    'phone_number_verified': None,
+    'address': None,
+    'updated_at': None              #Time the End-User's information was last updated.
+}
+
+SATOSA_TO_OIDC = dict((value, key) for key, value in OIDC_TO_SATOSA.items())
+
+PYSAML_TO_SATOSA = {
+    'aRecord': 'aRecord',
+    'aliasedEntryName': 'aliasedEntryName',
+    'aliasedObjectName': 'aliasedObjectName',
+    'associatedDomain': 'associatedDomain',
+    'associatedName': 'associatedName',
+    'audio': 'audio',
+    'authorityRevocationList': 'authorityRevocationList',
+    'buildingName': 'buildingName',
+    'businessCategory': 'businessCategory',
+    'c': 'c',
+    'cACertificate': 'cACertificate',
+    'cNAMERecord': 'cNAMERecord',
+    'carLicense': 'carLicense',
+    'certificateRevocationList': 'certificateRevocationList',
+    'cn': 'cn',
+    'co': 'co',
+    'commonName': 'commonName',
+    'countryName': 'countryName',
+    'crossCertificatePair': 'crossCertificatePair',
+    'dITRedirect': 'dITRedirect',
+    'dSAQuality': 'dSAQuality',
+    'dc': 'dc',
+    'deltaRevocationList': 'deltaRevocationList',
+    'departmentNumber': 'departmentNumber',
+    'description': 'description',
+    'destinationIndicator': 'destinationIndicator',
+    'displayName': 'displayName',
+    'distinguishedName': 'distinguishedName',
+    'dmdName': 'dmdName',
+    'dnQualifier': 'dnQualifier',
+    'documentAuthor': 'documentAuthor',
+    'documentIdentifier': 'documentIdentifier',
+    'documentLocation': 'documentLocation',
+    'documentPublisher': 'documentPublisher',
+    'documentTitle': 'documentTitle',
+    'documentVersion': 'documentVersion',
+    'domainComponent': 'domainComponent',
+    'drink': 'drink',
+    'eduOrgHomePageURI': 'eduOrgHomePageURI',
+    'eduOrgIdentityAuthNPolicyURI': 'eduOrgIdentityAuthNPolicyURI',
+    'eduOrgLegalName': 'eduOrgLegalName',
+    'eduOrgSuperiorURI': 'eduOrgSuperiorURI',
+    'eduOrgWhitePagesURI': 'eduOrgWhitePagesURI',
+    'eduPersonAffiliation': 'eduPersonAffiliation',
+    'eduPersonEntitlement': 'eduPersonEntitlement',
+    'eduPersonNickname': 'eduPersonNickname',
+    'eduPersonOrgDN': 'eduPersonOrgDN',
+    'eduPersonOrgUnitDN': 'eduPersonOrgUnitDN',
+    'eduPersonPrimaryAffiliation': 'eduPersonPrimaryAffiliation',
+    'eduPersonPrimaryOrgUnitDN': 'eduPersonPrimaryOrgUnitDN',
+    'eduPersonPrincipalName': 'eduPersonPrincipalName',
+    'eduPersonScopedAffiliation': 'eduPersonScopedAffiliation',
+    'eduPersonTargetedID': 'eduPersonTargetedID',
+    'email': 'email',
+    'emailAddress': 'emailAddress',
+    'employeeNumber': 'employeeNumber',
+    'employeeType': 'employeeType',
+    'enhancedSearchGuide': 'enhancedSearchGuide',
+    'facsimileTelephoneNumber': 'facsimileTelephoneNumber',
+    'favouriteDrink': 'favouriteDrink',
+    'fax': 'fax',
+    'federationFeideSchemaVersion': 'federationFeideSchemaVersion',
+    'friendlyCountryName': 'friendlyCountryName',
+    'generationQualifier': 'generationQualifier',
+    'givenName': 'givenName',
+    'gn': 'gn',
+    'homePhone': 'homePhone',
+    'homePostalAddress': 'homePostalAddress',
+    'homeTelephoneNumber': 'homeTelephoneNumber',
+    'host': 'host',
+    'houseIdentifier': 'houseIdentifier',
+    'info': 'info',
+    'initials': 'initials',
+    'internationaliSDNNumber': 'internationaliSDNNumber',
+    'janetMailbox': 'janetMailbox',
+    'jpegPhoto': 'jpegPhoto',
+    'knowledgeInformation': 'knowledgeInformation',
+    'l': 'l',
+    'labeledURI': 'labeledURI',
+    'localityName': 'localityName',
+    'mDRecord': 'mDRecord',
+    'mXRecord': 'mXRecord',
+    'mail': 'mail',
+    'mailPreferenceOption': 'mailPreferenceOption',
+    'manager': 'manager',
+    'member': 'member',
+    'mobile': 'mobile',
+    'mobileTelephoneNumber': 'mobileTelephoneNumber',
+    'nSRecord': 'nSRecord',
+    'name': 'name',
+    'norEduOrgAcronym': 'norEduOrgAcronym',
+    'norEduOrgNIN': 'norEduOrgNIN',
+    'norEduOrgSchemaVersion': 'norEduOrgSchemaVersion',
+    'norEduOrgUniqueIdentifier': 'norEduOrgUniqueIdentifier',
+    'norEduOrgUniqueNumber': 'norEduOrgUniqueNumber',
+    'norEduOrgUnitUniqueIdentifier': 'norEduOrgUnitUniqueIdentifier',
+    'norEduOrgUnitUniqueNumber': 'norEduOrgUnitUniqueNumber',
+    'norEduPersonBirthDate': 'norEduPersonBirthDate',
+    'norEduPersonLIN': 'norEduPersonLIN',
+    'norEduPersonNIN': 'norEduPersonNIN',
+    'o': 'o',
+    'objectClass': 'objectClass',
+    'organizationName': 'organizationName',
+    'organizationalStatus': 'organizationalStatus',
+    'organizationalUnitName': 'organizationalUnitName',
+    'otherMailbox': 'otherMailbox',
+    'ou': 'ou',
+    'owner': 'owner',
+    'pager': 'pager',
+    'pagerTelephoneNumber': 'pagerTelephoneNumber',
+    'personalSignature': 'personalSignature',
+    'personalTitle': 'personalTitle',
+    'photo': 'photo',
+    'physicalDeliveryOfficeName': 'physicalDeliveryOfficeName',
+    'pkcs9email': 'pkcs9email',
+    'postOfficeBox': 'postOfficeBox',
+    'postalAddress': 'postalAddress',
+    'postalCode': 'postalCode',
+    'preferredDeliveryMethod': 'preferredDeliveryMethod',
+    'preferredLanguage': 'preferredLanguage',
+    'presentationAddress': 'presentationAddress',
+    'protocolInformation': 'protocolInformation',
+    'pseudonym': 'pseudonym',
+    'registeredAddress': 'registeredAddress',
+    'rfc822Mailbox': 'rfc822Mailbox',
+    'roleOccupant': 'roleOccupant',
+    'roomNumber': 'roomNumber',
+    'sOARecord': 'sOARecord',
+    'searchGuide': 'searchGuide',
+    'secretary': 'secretary',
+    'seeAlso': 'seeAlso',
+    'serialNumber': 'serialNumber',
+    'singleLevelQuality': 'singleLevelQuality',
+    'sn': 'sn',
+    'st': 'st',
+    'stateOrProvinceName': 'stateOrProvinceName',
+    'street': 'street',
+    'streetAddress': 'streetAddress',
+    'subtreeMaximumQuality': 'subtreeMaximumQuality',
+    'subtreeMinimumQuality': 'subtreeMinimumQuality',
+    'supportedAlgorithms': 'supportedAlgorithms',
+    'supportedApplicationContext': 'supportedApplicationContext',
+    'surname': 'surname',
+    'telephoneNumber': 'telephoneNumber',
+    'teletexTerminalIdentifier': 'teletexTerminalIdentifier',
+    'telexNumber': 'telexNumber',
+    'textEncodedORAddress': 'textEncodedORAddress',
+    'title': 'title',
+    'uid': 'uid',
+    'uniqueIdentifier': 'uniqueIdentifier',
+    'uniqueMember': 'uniqueMember',
+    'userCertificate': 'userCertificate',
+    'userClass': 'userClass',
+    'userPKCS12': 'userPKCS12',
+    'userPassword': 'userPassword',
+    'userSMIMECertificate': 'userSMIMECertificate',
+    'userid': 'userid',
+    'x121Address': 'x121Address',
+    'x500UniqueIdentifier': 'x500UniqueIdentifier',
+}
+
+SATOSA_TO_PYSAML = dict((value, key) for key, value in PYSAML_TO_SATOSA.items())
+
+
+class UserIdHashType(Enum):
+    transient = 1
+    persistent = 1
+    pairwise = 1
+    public = 2
+
+
+class AuthenticationInformation():
+
+    def __init__(self, auth_class_ref, timestamp, issuer):
+        self.auth_class_ref = auth_class_ref
+        self.timestamp = timestamp
+        self.issuer = issuer
+
+
+class InternalData(object):
+    """
+    Holds internal representation of service related data.
+
+    :param _attributes: This dict is a data carrier between frontend and backend modules.
+
+    :type _user_id: str
+    :type _attributes: dict[str, str]
+    :type user_id_hash_type: UserIdHashType
+    :type internal_attributes: dict[str, str]
+    :type auth_info: AuthenticationInformation
+    """
+
+    def __init__(self, user_id_hash_type, internal_attributes=SATOSA_ATTRIBUTES, auth_info=None):
+        self._user_id = None
+        self._attributes = {}
+        self.user_id_hash_type = None
+        self.internal_attributes = internal_attributes
+        self.auth_info = auth_info
+
+    def add_pysaml_attributes(self, dict):
+        """
+        :type dict: dict[str, str]
+        :param dict:
+        :return:
+        """
+        self.add_attributes(PYSAML_TO_SATOSA, dict)
+
+    def get_pysaml_attributes(self):
+        return self.get_attributes(SATOSA_TO_PYSAML)
+
+    def get_attributes(self, map):
+        attributes = {}
+        for s_key in self._attributes:
+            if (s_key in map and s_key in self.internal_attributes and
+                    self.internal_attributes[s_key]):
+                attributes[SATOSA_TO_PYSAML[s_key]] = self._attributes[s_key]
+        return attributes
+
+    def add_attributes(self, map, dict):
+        """
+        :type dict: dict[str, str]
+        :param dict:
+        :return:
+        """
+        attributes = {}
+        for key in dict:
+            if key in map:
+                s_key = map[key]
+                if s_key in self.internal_attributes and self.internal_attributes[s_key]:
+                    self._attributes[s_key] = dict[key]
+
+    @property
+    def user_id(self):
+        """
+        Get the user identification.
+
+        :rtype: str
+
+        :return: User identification.
+        """
+        return self._user_id
+
+    @user_id.setter
+    def user_id(self, user_id):
+        """
+        Inserts a path to the context.
+        This path is striped by the base_url, so for example:
+            A path BASE_URL/ENDPOINT_URL, would be inserted as only ENDPOINT_URL
+            https://localhost:8092/sso/redirect -> sso/redirect
+
+        :type p: str
+
+        :param p: A path to an endpoint.
+        :return: None
+        """
+        if not user_id:
+            raise ValueError("user_id can't be set to None")
+        elif p.startswith('/'):
+            raise ValueError("path can't start with '/'")
+        self._user_id = p
