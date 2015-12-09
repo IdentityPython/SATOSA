e85535d479af6ac59385c3afb74edb80dc60cfe3
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-12-09 13:45:53 +0100

    Added tests for DataConverter.

--- tests/satosa/test_internal_data.py
+++ tests/satosa/test_internal_data.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from satosa.internal_data import DataConverter
 
 
@@ -87,3 +89,64 @@ class TestDataConverter:
         internal_repr = converter.to_internal("openid", data)
         external_repr = converter.from_internal("saml", internal_repr)
         assert external_repr["postaladdress"] == [data["address"]["formatted"]["text"]["value"]]
+
+    def test_mapping_to_nested_attribute(self):
+        mapping = {
+            "attributes": {
+                "address": {
+                    "openid": ["address.formatted"],
+                    "saml": ["postaladdress"]
+                },
+            },
+        }
+
+        data = {
+            "postaladdress": "100 Universal City Plaza, Hollywood CA 91608, USA"
+        }
+
+        converter = DataConverter(mapping)
+        internal_repr = converter.to_internal("saml", data)
+        external_repr = converter.from_internal("openid", internal_repr)
+        assert external_repr["address"]["formatted"] == [data["postaladdress"]]
+
+    def test_mapping_to_deeply_nested_attribute(self):
+        mapping = {
+            "attributes": {
+                "address": {
+                    "openid": ["address.formatted.text.value"],
+                    "saml": ["postaladdress"]
+                },
+            },
+        }
+
+        data = {
+            "postaladdress": "100 Universal City Plaza, Hollywood CA 91608, USA"
+        }
+
+        converter = DataConverter(mapping)
+        internal_repr = converter.to_internal("saml", data)
+        external_repr = converter.from_internal("openid", internal_repr)
+        assert external_repr["address"]["formatted"]["text"]["value"] == [data["postaladdress"]]
+
+    def test_multiple_source_attribute_values(self):
+        mapping = {
+            "attributes": {
+                "mail": {
+                    "saml": ["mail", "emailAddress", "email"]
+                },
+            },
+        }
+
+        data = {
+            "mail": "test1@example.com",
+            "email": "test2@example.com",
+            "emailAddress": "test3@example.com",
+        }
+
+        expected = Counter(["test1@example.com", "test2@example.com", "test3@example.com"])
+
+        converter = DataConverter(mapping)
+        internal_repr = converter.to_internal("saml", data)
+        assert Counter(internal_repr["mail"]) == expected
+        external_repr = converter.from_internal("saml", internal_repr)
+        assert Counter(external_repr[mapping["attributes"]["mail"]["saml"][0]]) == expected
