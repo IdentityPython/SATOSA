8a018494d5ab9e29942d5403b37b3c43c4ebed57
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-01-05 13:14:57 +0100

    Refactor and cleanup SAML frontend tests.

--- tests/satosa/frontends/test_saml2.py
+++ tests/satosa/frontends/test_saml2.py
@@ -17,7 +17,7 @@ from satosa.internal_data import InternalResponse, AuthenticationInformation, In
 from satosa.state import State
 from satosa.util import saml_name_format_to_hash_type
 from tests.users import USERS
-from tests.util import FakeSP, FileGenerator, create_metadata_from_config_dict
+from tests.util import FakeSP, create_metadata_from_config_dict
 
 INTERNAL_ATTRIBUTES = {
     'attributes': {'displayname': {'openid': ['nickname'], 'saml': ['displayName']},
@@ -31,126 +31,130 @@ INTERNAL_ATTRIBUTES = {
                    'surname': {'saml': ['sn', 'surname'], 'openid': ['family_name'],
                                'facebook': ['last_name']}}}
 
-IDP_STATE_ID = "frontend_idp"
-IDP_BASE = "http://test.tester.se"
-RECEIVER = "Saml2IDP"
 ENDPOINTS = {"single_sign_on_service": {BINDING_HTTP_REDIRECT: "sso/redirect",
                                         BINDING_HTTP_POST: "sso/post"}}
 
-SP_BASE = "http://example.com"
 
+class TestSamlFrontend:
+    def construct_base_url_from_entity_id(self, entity_id):
+        return "{parsed.scheme}://{parsed.netloc}".format(parsed=urlparse(entity_id))
 
-@pytest.mark.parametrize("conf", [
-    None,
-    {"idp_config_notok": {}, "endpoints": {}, "base": "base",
-     "state_id": "state_id"},
-    {"idp_config": {}, "endpoints_notok": {}, "base": "base",
-     "state_id": "state_id"},
-    {"idp_config": {}, "endpoints": {}, "base_notok": "base",
-     "state_id": "state_id"}
-])
-def test_config_error_handling(conf):
-    with pytest.raises(AssertionError):
-        SamlFrontend(lambda ctx, req: None, INTERNAL_ATTRIBUTES, conf)
+    @pytest.mark.parametrize("conf", [
+        None,
+        {"idp_config_notok": {}, "endpoints": {}, "base": "base",
+         "state_id": "state_id"},
+        {"idp_config": {}, "endpoints_notok": {}, "base": "base",
+         "state_id": "state_id"},
+        {"idp_config": {}, "endpoints": {}, "base_notok": "base",
+         "state_id": "state_id"}
+    ])
+    def test_config_error_handling(self, conf):
+        with pytest.raises(AssertionError):
+            SamlFrontend(lambda ctx, req: None, INTERNAL_ATTRIBUTES, conf)
 
+    def test_register_endpoints(self, idp_conf):
+        config = {"idp_config": idp_conf, "endpoints": ENDPOINTS,
+                  "base": self.construct_base_url_from_entity_id(idp_conf["entityid"]),
+                  "state_id": "state_id"}
+        samlfrontend = SamlFrontend(lambda context, internal_req: (context, internal_req),
+                                    INTERNAL_ATTRIBUTES, config)
 
-def test_handle_authn_request(idp_conf, sp_conf):
-    """
-    Performs a complete test for the module. The flow should be accepted.
-    """
-    base = "{parsed.scheme}://{parsed.netloc}".format(parsed=urlparse(idp_conf["entityid"]))
-    CONFIG = {"idp_config": idp_conf, "endpoints": ENDPOINTS, "base": base,
-              "state_id": IDP_STATE_ID}
-    providers = ["foo", "bar"]
-    sp_metadata_str = create_metadata_from_config_dict(sp_conf)
-    idp_conf["metadata"]["inline"] = [sp_metadata_str]
-
-    samlfrontend = SamlFrontend(lambda context, internal_req: (context, internal_req),
-                                INTERNAL_ATTRIBUTES, CONFIG)
-
-    url_map = samlfrontend.register_endpoints(providers)
-    for regex in url_map:
-        p = re.compile(regex[0])
-        match = False
-        for provider in providers:
-            for s_key in CONFIG["endpoints"]:
-                for b_key in CONFIG["endpoints"][s_key]:
-                    if p.match(provider + "/" + CONFIG["endpoints"][s_key][b_key]) or \
-                            p.match(provider + "/" + CONFIG["endpoints"][s_key][b_key] + "/test"):
+        providers = ["foo", "bar"]
+        url_map = samlfrontend.register_endpoints(providers)
+        for k, v in idp_conf["service"]["idp"]["endpoints"].items():
+            for endp in v:
+                match = False
+                for regex in url_map:
+                    p = re.compile(regex[0])
+                    if p.match(urlparse(endp[0]).path.lstrip("/")):
                         match = True
                         break
-        assert match, "All regular expressions must match!"
+                assert match, "Not correct regular expression for endpoint: %s" % endp[0]
 
-    idp_metadata_str = create_metadata_from_config_dict(samlfrontend.config)
-    sp_conf["metadata"]["inline"].append(idp_metadata_str)
-    fakesp = FakeSP(None, config=SPConfig().load(sp_conf, metadata_construction=False))
-    context = Context()
-    context.state = State()
-    context.request = parse.parse_qs(
-            urlparse(fakesp.make_auth_req(samlfrontend.config["entityid"])).query)
-    tmp_dict = {}
-    for val in context.request:
-        if isinstance(context.request[val], list):
-            tmp_dict[val] = context.request[val][0]
-        else:
-            tmp_dict[val] = context.request[val]
-    context.request = tmp_dict
+    def test_handle_authn_request(self, idp_conf, sp_conf):
+        """
+        Performs a complete test for the module. The flow should be accepted.
+        """
+        base = self.construct_base_url_from_entity_id(idp_conf["entityid"])
+        config = {"idp_config": idp_conf, "endpoints": ENDPOINTS, "base": base,
+                  "state_id": "state_id"}
+        sp_metadata_str = create_metadata_from_config_dict(sp_conf)
+        idp_conf["metadata"]["inline"] = [sp_metadata_str]
 
-    _, internal_req = samlfrontend.handle_authn_request(context, BINDING_HTTP_REDIRECT)
-    assert internal_req.requestor == sp_conf["entityid"]
+        samlfrontend = SamlFrontend(lambda context, internal_req: (context, internal_req),
+                                    INTERNAL_ATTRIBUTES, config)
+        samlfrontend.register_endpoints(["saml"])
 
-    auth_info = AuthenticationInformation(PASSWORD, "2015-09-30T12:21:37Z", "unittest_idp.xml")
-    internal_response = InternalResponse(auth_info=auth_info)
-    internal_response.set_user_id_hash_type(internal_req.user_id_hash_type)
-    internal_response.add_attributes(USERS["testuser1"])
+        idp_metadata_str = create_metadata_from_config_dict(samlfrontend.config)
+        sp_conf["metadata"]["inline"].append(idp_metadata_str)
+        fakesp = FakeSP(None, config=SPConfig().load(sp_conf, metadata_construction=False))
+        context = Context()
+        context.state = State()
+        context.request = parse.parse_qs(
+                urlparse(fakesp.make_auth_req(samlfrontend.config["entityid"])).query)
+        tmp_dict = {}
+        for val in context.request:
+            if isinstance(context.request[val], list):
+                tmp_dict[val] = context.request[val][0]
+            else:
+                tmp_dict[val] = context.request[val]
+        context.request = tmp_dict
 
-    resp = samlfrontend.handle_authn_response(context, internal_response)
-    resp_dict = parse_qs(urlparse(resp.message).query)
-    resp = fakesp.parse_authn_request_response(resp_dict['SAMLResponse'][0],
-                                               BINDING_HTTP_REDIRECT)
-    for key in resp.ava:
-        assert USERS["testuser1"][key] == resp.ava[key]
+        _, internal_req = samlfrontend.handle_authn_request(context, BINDING_HTTP_REDIRECT)
+        assert internal_req.requestor == sp_conf["entityid"]
 
+        auth_info = AuthenticationInformation(PASSWORD, "2015-09-30T12:21:37Z", "unittest_idp.xml")
+        internal_response = InternalResponse(auth_info=auth_info)
+        internal_response.set_user_id_hash_type(internal_req.user_id_hash_type)
+        internal_response.add_attributes(USERS["testuser1"])
 
-def test_get_filter_attributes_with_sp_requested_attributes_without_friendlyname(idp_conf):
-    sp_metadata_str = """<?xml version="1.0"?>
-    <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="http://sp.example.com">
-      <md:SPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:1.1:protocol urn:oasis:names:tc:SAML:2.0:protocol">
-        <md:AttributeConsumingService>
-          <md:RequestedAttribute Name="urn:oid:1.3.6.1.4.1.5923.1.1.1.10" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri" isRequired="true"/>
-          <md:RequestedAttribute Name="urn:oid:1.3.6.1.4.1.5923.1.1.1.6" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri" isRequired="true"/>
-          <md:RequestedAttribute Name="urn:oid:1.3.6.1.4.1.5923.1.1.1.1" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"/>
-          <md:RequestedAttribute Name="urn:oid:0.9.2342.19200300.100.1.3" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri" isRequired="true"/>
-          <md:RequestedAttribute Name="urn:oid:2.16.840.1.113730.3.1.241" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"/>
-          <md:RequestedAttribute Name="urn:oid:2.5.4.4" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"/>
-          <md:RequestedAttribute Name="urn:oid:2.5.4.42" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"/>
-        </md:AttributeConsumingService>
-      </md:SPSSODescriptor>
-    </md:EntityDescriptor>
-    """
+        resp = samlfrontend.handle_authn_response(context, internal_response)
+        resp_dict = parse_qs(urlparse(resp.message).query)
+        resp = fakesp.parse_authn_request_response(resp_dict['SAMLResponse'][0],
+                                                   BINDING_HTTP_REDIRECT)
+        for key in resp.ava:
+            assert USERS["testuser1"][key] == resp.ava[key]
 
-    idp_conf["metadata"] = {"inline": [sp_metadata_str]}
+    def test_get_filter_attributes_with_sp_requested_attributes_without_friendlyname(self,
+                                                                                     idp_conf):
+        sp_metadata_str = """<?xml version="1.0"?>
+        <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="http://sp.example.com">
+          <md:SPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:1.1:protocol urn:oasis:names:tc:SAML:2.0:protocol">
+            <md:AttributeConsumingService>
+              <md:RequestedAttribute Name="urn:oid:1.3.6.1.4.1.5923.1.1.1.10" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri" isRequired="true"/>
+              <md:RequestedAttribute Name="urn:oid:1.3.6.1.4.1.5923.1.1.1.6" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri" isRequired="true"/>
+              <md:RequestedAttribute Name="urn:oid:1.3.6.1.4.1.5923.1.1.1.1" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"/>
+              <md:RequestedAttribute Name="urn:oid:0.9.2342.19200300.100.1.3" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri" isRequired="true"/>
+              <md:RequestedAttribute Name="urn:oid:2.16.840.1.113730.3.1.241" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"/>
+              <md:RequestedAttribute Name="urn:oid:2.5.4.4" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"/>
+              <md:RequestedAttribute Name="urn:oid:2.5.4.42" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"/>
+            </md:AttributeConsumingService>
+          </md:SPSSODescriptor>
+        </md:EntityDescriptor>
+        """
 
-    base = "{parsed.scheme}://{parsed.netloc}".format(parsed=urlparse(idp_conf["entityid"]))
-    conf = {"idp_config": idp_conf, "endpoints": ENDPOINTS, "base": base,
-            "state_id": IDP_STATE_ID}
+        idp_conf["metadata"] = {"inline": [sp_metadata_str]}
 
-    internal_attributes = {"attributes": {attr_name: {"saml": [attr_name]} for attr_name in
-                                          ["edupersontargetedid", "edupersonprincipalname",
-                                           "edupersonaffiliation", "mail", "displayname", "sn",
-                                           "givenname"]}}  # no op mapping for saml attribute names
+        base = self.construct_base_url_from_entity_id(idp_conf["entityid"])
+        conf = {"idp_config": idp_conf, "endpoints": ENDPOINTS, "base": base,
+                "state_id": "state_id"}
 
-    samlfrontend = SamlFrontend(None, internal_attributes, conf)
-    samlfrontend.register_endpoints(["testprovider"])
+        internal_attributes = {"attributes": {attr_name: {"saml": [attr_name]} for attr_name in
+                                              ["edupersontargetedid", "edupersonprincipalname",
+                                               "edupersonaffiliation", "mail", "displayname", "sn",
+                                               "givenname"]}}  # no op mapping for saml attribute names
 
-    internal_req = InternalRequest(saml_name_format_to_hash_type(NAMEID_FORMAT_PERSISTENT),
-                                   "http://sp.example.com",
-                                   "Example SP")
-    filtered_attributes = samlfrontend.get_filter_attributes(samlfrontend.idp,
-                                                             samlfrontend.idp.config.getattr(
-                                                                     "policy", "idp"),
-                                                             internal_req.requestor, None)
+        samlfrontend = SamlFrontend(None, internal_attributes, conf)
+        samlfrontend.register_endpoints(["testprovider"])
 
-    assert set(filtered_attributes) == set(
-            ["edupersontargetedid", "edupersonprincipalname", "edupersonaffiliation", "mail",
-             "displayname", "sn", "givenname"])
+        internal_req = InternalRequest(saml_name_format_to_hash_type(NAMEID_FORMAT_PERSISTENT),
+                                       "http://sp.example.com",
+                                       "Example SP")
+        filtered_attributes = samlfrontend.get_filter_attributes(samlfrontend.idp,
+                                                                 samlfrontend.idp.config.getattr(
+                                                                         "policy", "idp"),
+                                                                 internal_req.requestor, None)
+
+        assert set(filtered_attributes) == set(
+                ["edupersontargetedid", "edupersonprincipalname", "edupersonaffiliation", "mail",
+                 "displayname", "sn", "givenname"])
