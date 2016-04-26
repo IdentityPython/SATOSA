30001f51a4d17835585e9953b5c113d047d7a102
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-04-26 06:51:37 +0200

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SATOSA',
-    version='1.0.3',
+    version='1.0.4',
     description='Protocol proxy (SAML/OIDC).',
     author='DIRG',
     author_email='dirg@its.umu.se',
