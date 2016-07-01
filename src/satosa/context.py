3fad0970e8b3537b346fc8ffe860d450b6552053
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-07-01 16:39:06 +0200

    Remove unnecessary properties in Context.

--- src/satosa/context.py
+++ src/satosa/context.py
@@ -19,46 +19,12 @@ class Context(object):
     def __init__(self):
         self._path = None
         self.request = None
-        self._target_backend = None
-        self._target_frontend = None
+        self.target_backend = None
+        self.target_frontend = None
         # This dict is a data carrier between frontend and backend modules.
         self.internal_data = {}
         self.cookie = None
         self.state = None
-
-    @property
-    def target_backend(self):
-        """
-        :rtype: str
-        :return: Target backend
-        """
-        return self._target_backend
-
-    @target_backend.setter
-    def target_backend(self, t):
-        """
-        Set target backend
-        :type t: str
-        :param t: Target backend
-        """
-        self._target_backend = t
-
-    @property
-    def target_frontend(self):
-        """
-        :rtype: str
-        :return: Target frontend
-        """
-        return self._target_frontend
-
-    @target_frontend.setter
-    def target_frontend(self, t):
-        """
-        Set target frontend
-        :type t: str
-        :param t: Target frontend
-        """
-        self._target_frontend = t
 
     @property
     def path(self):
