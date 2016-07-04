f7d47057389a1edeec9fa35fb6a2dbc6b8de5fe6
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-07-04 08:55:25 +0200

    Refactor routing.py.

--- src/satosa/routing.py
+++ src/satosa/routing.py
@@ -10,6 +10,8 @@ from .logging_util import satosa_logging
 
 logger = logging.getLogger(__name__)
 
+STATE_KEY = "ROUTER"
+
 
 class SATOSANoBoundEndpointError(SATOSAError):
     """
@@ -31,8 +33,6 @@ class ModuleRouter(object):
     and handles the internal routing between frontends and backends.
     """
 
-    STATE_KEY = "ROUTER"
-
     def __init__(self, frontends, backends):
         """
         :type frontends: dict[str, satosa.frontends.base.FrontendModule]
@@ -44,21 +44,13 @@ class ModuleRouter(object):
         module
         """
 
-        if not (frontends and backends):
-            raise ValueError("Need at least one frontend and backend")
+        if not frontends and not backends:
+            raise ValueError("Need at least one frontend and one backend")
 
-        self.frontends = {}
-        self.backends = {}
-
-        for backend in backends:
-            self.backends[backend] = {"instance": backends[backend],
-                                      "endpoints": backends[backend].register_endpoints()}
-
-        providers = list(backends.keys())
-        for frontend in frontends:
-            self.frontends[frontend] = {"instance": frontends[frontend],
-                                        "endpoints": frontends[frontend].register_endpoints(
-                                            providers)}
+        self.frontends = {name: {"instance": instance, "endpoints": instance.register_endpoints(backends.keys())}
+                          for name, instance in frontends.items()}
+        self.backends = {name: {"instance": instance, "endpoints": instance.register_endpoints()}
+                         for name, instance in backends.items()}
 
         logger.debug("Loaded backends with endpoints: %s" % backends)
         logger.debug("Loaded frontends with endpoints: %s" % frontends)
@@ -73,11 +65,9 @@ class ModuleRouter(object):
         :param context: The request context
         :return: backend
         """
-        state = context.state
-        satosa_logging(logger, logging.INFO, "Routing to backend: %s " % context.target_backend,
-                       state)
+        satosa_logging(logger, logging.DEBUG, "Routing to backend: %s " % context.target_backend, context.state)
         backend = self.backends[context.target_backend]["instance"]
-        state[ModuleRouter.STATE_KEY] = context.target_frontend
+        context.state[STATE_KEY] = context.target_frontend
         return backend
 
     def frontend_routing(self, context):
@@ -91,30 +81,41 @@ class ModuleRouter(object):
         :return: frontend
         """
 
-        state = context.state
-        target_frontend = state[ModuleRouter.STATE_KEY]
-        satosa_logging(logger, logging.INFO, "Routing to frontend: %s " % target_frontend, state)
+        target_frontend = context.state[STATE_KEY]
+        satosa_logging(logger, logging.DEBUG, "Routing to frontend: %s " % target_frontend, context.state)
         context.target_frontend = target_frontend
         frontend = self.frontends[context.target_frontend]["instance"]
         return frontend
 
-    def _validate_context(self, context):
-        """
-        Validates values in the context needed by the ModuleRouter.
-        Raises BadContextError if any error.
+    def _find_registered_frontend_endpoint(self, context):
+        # Search for frontend endpoint
+        for frontend in self.frontends.keys():
+            for regex, spec in self.frontends[frontend]["endpoints"]:
+                match = re.search(regex, context.path)
+                if match is not None:
+                    context.target_frontend = frontend
+                    msg = "Frontend request. Module name:'{name}', endpoint: {endpoint}".format(
+                        name=frontend,
+                        endpoint=context.path)
+                    satosa_logging(logger, logging.DEBUG, msg, context.state)
+                    return spec
 
-        :type context: satosa.context.Context
-        :rtype: None
+        return None
 
-        :param context: The request context
-        :return: None
-        """
-        if not context:
-            satosa_logging(logger, logging.DEBUG, "Context was None!", context.state)
-            raise SATOSABadContextError("Context is None")
-        if context.path is None:
-            satosa_logging(logger, logging.DEBUG, "Context did not contain a path!", context.state)
-            raise SATOSABadContextError("Context did not contain any path")
+    def _find_registered_backend_endpoint(self, context):
+        # Search for backend endpoint
+        for regex, spec in self.backends[context.target_backend]["endpoints"]:
+            match = re.search(regex, context.path)
+            if match is not None:
+                msg = "Backend request. Module name:'{name}', endpoint: {endpoint}".format(
+                    name=context.target_backend,
+                    endpoint=context.path)
+                satosa_logging(logger, logging.DEBUG, msg, context.state)
+                return spec
+
+        satosa_logging(logger, logging.DEBUG, "%s not bound to any function" % context.path,
+                       context.state)
+        return None
 
     def endpoint_routing(self, context):
         """
@@ -126,40 +127,26 @@ class ModuleRouter(object):
         :param context: The request context
         :return: registered endpoint and bound parameters
         """
-        satosa_logging(logger, logging.DEBUG, "Routing path: %s" % context.path, context.state)
-        self._validate_context(context)
+        if context.path is None:
+            satosa_logging(logger, logging.DEBUG, "Context did not contain a path!", context.state)
+            raise SATOSABadContextError("Context did not contain any path")
 
-        path_split = context.path.split('/')
+        satosa_logging(logger, logging.DEBUG, "Routing path: %s" % context.path, context.state)
+        path_split = context.path.split("/")
         backend = path_split[0]
 
         if backend in self.backends:
             context.target_backend = backend
-
-        # Search for frontend endpoint
-        for frontend in self.frontends.keys():
-            for regex, spec in self.frontends[frontend]["endpoints"]:
-                match = re.search(regex, context.path)
-                if match is not None:
-                    context.target_frontend = frontend
-                    msg = "Frontend request. Module name:'{name}', endpoint: {endpoint}".format(
-                        name=frontend,
-                        endpoint=context.path)
-                    satosa_logging(logger, logging.INFO, msg, context.state)
-                    return spec
-
-        if backend in self.backends:
-            # Search for backend endpoint
-            for regex, spec in self.backends[backend]["endpoints"]:
-                match = re.search(regex, context.path)
-                if match is not None:
-                    msg = "Backend request. Module name:'{name}', endpoint: {endpoint}".format(
-                        name=backend,
-                        endpoint=context.path)
-                    satosa_logging(logger, logging.INFO, msg, context.state)
-                    return spec
-            satosa_logging(logger, logging.DEBUG, "%s not bound to any function" % context.path,
-                           context.state)
         else:
             satosa_logging(logger, logging.DEBUG, "Unknown backend %s" % backend, context.state)
+
+        frontend_endpoint = self._find_registered_frontend_endpoint(context)
+        if frontend_endpoint:
+            return frontend_endpoint
+
+        if backend in self.backends:
+            backend_endpoint = self._find_registered_backend_endpoint(context)
+            if backend_endpoint:
+                return backend_endpoint
 
         raise SATOSANoBoundEndpointError("'{}' not bound to any function".format(context.path))
