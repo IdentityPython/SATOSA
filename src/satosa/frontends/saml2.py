0d72d962d2eab38f261dc1cef1758587a3e20ee1
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-08-15 19:46:53 +0200

    Fix typo in keyword argument in partial function.
    The correct arg name is 'binding_in', which if specified incorrectly as
    'binding' will throw an exception.

--- src/satosa/frontends/saml2.py
+++ src/satosa/frontends/saml2.py
@@ -322,7 +322,7 @@ class SAMLFrontend(FrontendModule):
                 valid_providers = valid_providers.lstrip("|")
                 parsed_endp = urlparse(endp)
                 url_map.append(("%s/%s$" % (valid_providers, parsed_endp.path),
-                                functools.partial(self.handle_authn_request, binding=binding)))
+                                functools.partial(self.handle_authn_request, binding_in=binding)))
 
         if "publish_metadata" in self.config:
             metadata_path = urlparse(self.config["publish_metadata"])
@@ -496,6 +496,6 @@ class SAMLMirrorFrontend(SAMLFrontend):
                 valid_providers = valid_providers.lstrip("|")
                 parsed_endp = urlparse(endp)
                 url_map.append(("%s/[\s\S]+/%s/?.*$" % (valid_providers, parsed_endp.path),
-                                functools.partial(self.handle_authn_request, binding=binding)))
+                                functools.partial(self.handle_authn_request, binding_in=binding)))
 
         return url_map
