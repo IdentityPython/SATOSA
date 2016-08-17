abd602d3eb51d6e785d8774fb43dc7368cb12958
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-08-17 09:44:43 +0200

    Make SAML config fixtures global in conftest.py.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,17 @@
+import copy
 import os
 
 import pytest
-from saml2 import BINDING_HTTP_REDIRECT
+from saml2 import BINDING_HTTP_REDIRECT, BINDING_HTTP_POST
 from saml2.extension.idpdisc import BINDING_DISCO
 from saml2.saml import NAME_FORMAT_URI, NAMEID_FORMAT_TRANSIENT, NAMEID_FORMAT_PERSISTENT
 
 from satosa.context import Context
 from satosa.state import State
+from .util import create_metadata_from_config_dict
 from .util import generate_cert, write_cert
 
+BASE_URL = "https://test-proxy.com"
 
 @pytest.fixture(scope="session")
 def signing_key_path(tmpdir_factory):
@@ -122,7 +125,7 @@ def context():
 def satosa_config_dict(backend_plugin_config, frontend_plugin_config, request_microservice_config,
                        response_microservice_config):
     config = {
-        "BASE": "https://example.com",
+        "BASE": BASE_URL,
         "COOKIE_STATE_NAME": "TEST_STATE",
         "BACKEND_MODULES": ["foo"],
         "FRONTEND_MODULES": ["bar"],
@@ -173,4 +176,92 @@ def response_microservice_config():
         "name": "response-microservice",
         "conf": {"qwe": "rty"}
     }
+    return data
+
+
+@pytest.fixture
+def saml_frontend_config(cert_and_key, sp_conf):
+    data = {
+        "module": "satosa.frontends.saml2.SAMLFrontend",
+        "name": "SAML2Frontend",
+        "config": {
+            "idp_config": {
+                "entityid": "frontend-entity_id",
+                "organization": {"display_name": "Test Identities", "name": "Test Identities Org.",
+                                 "url": "http://www.example.com"},
+                "contact_person": [{"contact_type": "technical", "email_address": "technical@example.com",
+                                    "given_name": "Technical"},
+                                   {"contact_type": "support", "email_address": "support@example.com",
+                                    "given_name": "Support"}],
+                "service": {
+                    "idp": {
+                        "endpoints": {
+                            "single_sign_on_service": []
+                        },
+                        "name": "Frontend IdP",
+                        "name_id_format": NAMEID_FORMAT_TRANSIENT,
+                        "policy": {
+                            "default": {
+                                "attribute_restrictions": None,
+                                "fail_on_missing_requested": False,
+                                "lifetime": {"minutes": 15},
+                                "name_form": NAME_FORMAT_URI
+                            }
+                        }
+                    }
+                },
+                "cert_file": cert_and_key[0],
+                "key_file": cert_and_key[1],
+                "metadata": {"inline": [create_metadata_from_config_dict(sp_conf)]}
+            },
+
+            "endpoints": {
+                "single_sign_on_service": {BINDING_HTTP_POST: "sso/post",
+                                           BINDING_HTTP_REDIRECT: "sso/redirect"}
+            }
+        }
+    }
+
+    return data
+
+
+@pytest.fixture
+def saml_backend_config(idp_conf):
+    name = "SAML2Backend"
+    data = {
+        "module": "satosa.backends.saml2.SAMLBackend",
+        "name": name,
+        "config": {
+            "sp_config": {
+                "entityid": "backend-entity_id",
+                "organization": {"display_name": "Example Identities", "name": "Test Identities Org.",
+                                 "url": "http://www.example.com"},
+                "contact_person": [
+                    {"contact_type": "technical", "email_address": "technical@example.com",
+                     "given_name": "Technical"},
+                    {"contact_type": "support", "email_address": "support@example.com", "given_name": "Support"}
+                ],
+                "service": {
+                    "sp": {
+                        "allow_unsolicited": True,
+                        "endpoints": {
+                            "assertion_consumer_service": [
+                                ("{}/{}/acs/redirect".format(BASE_URL, name), BINDING_HTTP_REDIRECT)],
+                            "discovery_response": [("{}/disco", BINDING_DISCO)]
+
+                        }
+                    }
+                },
+                "metadata": {"inline": [create_metadata_from_config_dict(idp_conf)]}
+            }
+        }
+    }
+    return data
+
+
+@pytest.fixture
+def saml_mirror_frontend_config(saml_frontend_config):
+    data = copy.deepcopy(saml_frontend_config)
+    data["module"] = "satosa.frontends.saml2.SAMLMirrorFrontend"
+    data["name"] = "SAMLMirrorFrontend"
     return data
--- tests/satosa/metadata_creation/test_saml_metadata.py
+++ tests/satosa/metadata_creation/test_saml_metadata.py
@@ -2,99 +2,19 @@ import copy
 from base64 import urlsafe_b64encode
 
 import pytest
-from saml2 import BINDING_HTTP_POST, BINDING_HTTP_REDIRECT
 from saml2.config import SPConfig, Config
-from saml2.extension.idpdisc import BINDING_DISCO
 from saml2.mdstore import InMemoryMetaData
 from saml2.metadata import entity_descriptor
-from saml2.saml import NAMEID_FORMAT_TRANSIENT, NAME_FORMAT_URI
 from saml2.sigver import security_context
 from saml2.time_util import in_a_while
 
 from satosa.metadata_creation.saml_metadata import create_entity_descriptors, create_signed_entities_descriptor
 from satosa.satosa_config import SATOSAConfig
+from tests.conftest import BASE_URL
 from tests.util import create_metadata_from_config_dict
-
-BASE_URL = "https://example.com"
 
 
 class TestCreateEntityDescriptors:
-    @pytest.fixture
-    def saml_backend_config(self):
-        data = {
-            "module": "satosa.backends.saml2.SAMLBackend",
-            "name": "SAML2Backend",
-            "config": {
-                "sp_config": {
-                    "entityid": "backend-entity_id",
-                    "organization": {"display_name": "Example Identities", "name": "Test Identities Org.",
-                                     "url": "http://www.example.com"},
-                    "contact_person": [
-                        {"contact_type": "technical", "email_address": "technical@example.com",
-                         "given_name": "Technical"},
-                        {"contact_type": "support", "email_address": "support@example.com", "given_name": "Support"}
-                    ],
-                    "service": {
-                        "sp": {
-                            "allow_unsolicited": True,
-                            "endpoints": {
-                                "assertion_consumer_service": [
-                                    ("{}/acs/post".format(BASE_URL), BINDING_HTTP_POST),
-                                    ("{}/acs/redirect", BINDING_HTTP_REDIRECT)],
-                                "discovery_response": [("{}/disco", BINDING_DISCO)]
-
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return data
-
-    @pytest.fixture
-    def saml_frontend_config(self):
-        data = {
-            "module": "satosa.frontends.saml2.SAMLFrontend",
-            "name": "SAML2Frontend",
-            "config": {
-                "idp_config": {
-                    "entityid": "frontend-entity_id",
-                    "organization": {"display_name": "Test Identities", "name": "Test Identities Org.",
-                                     "url": "http://www.example.com"},
-                    "contact_person": [{"contact_type": "technical", "email_address": "technical@example.com",
-                                        "given_name": "Technical"},
-                                       {"contact_type": "support", "email_address": "support@example.com",
-                                        "given_name": "Support"}],
-                    "service": {
-                        "idp": {
-                            "endpoints": {
-                                "single_sign_on_service": []
-                            },
-                            "name": "Frontend IdP",
-                            "name_id_format": NAMEID_FORMAT_TRANSIENT,
-                            "policy": {
-                                "default": {
-                                    "attribute_restrictions": None,
-                                    "fail_on_missing_requested": False,
-                                    "lifetime": {"minutes": 15},
-                                    "name_form": NAME_FORMAT_URI
-                                }
-                            }}}},
-                "endpoints": {
-                    "single_sign_on_service": {BINDING_HTTP_POST: "sso/post",
-                                               BINDING_HTTP_REDIRECT: "sso/redirect"}
-                }
-            }
-        }
-        return data
-
-    @pytest.fixture
-    def saml_mirror_frontend_config(self, saml_frontend_config):
-        data = copy.deepcopy(saml_frontend_config)
-        data["module"] = "satosa.frontends.saml2.SAMLMirrorFrontend"
-        data["name"] = "SAMLMirrorFrontend"
-        return data
-
     @pytest.fixture
     def oidc_backend_config(self):
         data = {
