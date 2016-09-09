94061e19800b6390cb88093f886a11705592c2f8
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-09-09 13:24:34 +0200

    Add integration test of account linking module.

---
+++ tests/flows/test_account_linking.py
@@ -0,0 +1,43 @@
+import responses
+from werkzeug.test import Client
+from werkzeug.wrappers import BaseResponse
+
+from satosa.proxy_server import make_app
+from satosa.satosa_config import SATOSAConfig
+
+
+class TestAccountLinking:
+    def test_full_flow(self, satosa_config_dict, signing_key_path):
+        api_url = "https://alservice.example.com/api"
+        redirect_url = "https://alservice.examle.com/redirect"
+        satosa_config_dict["ACCOUNT_LINKING"] = {
+            "enable": True,
+            "api_url": api_url,
+            "redirect_url": redirect_url,
+            "sign_key": signing_key_path
+        }
+
+        # application
+        test_client = Client(make_app(SATOSAConfig(satosa_config_dict)), BaseResponse)
+
+        # incoming auth req
+        http_resp = test_client.get("/{}/{}/request".format(satosa_config_dict["BACKEND_MODULES"][0]["name"],
+                                                            satosa_config_dict["FRONTEND_MODULES"][0]["name"]))
+        assert http_resp.status_code == 200
+
+        with responses.RequestsMock() as rsps:
+            # fake no previous account linking
+            rsps.add(responses.GET, "{}/get_id".format(api_url), "test_ticket", status=404)
+
+            # incoming auth resp
+            http_resp = test_client.get("/{}/response".format(satosa_config_dict["BACKEND_MODULES"][0]["name"]))
+            assert http_resp.status_code == 302
+            assert http_resp.headers["Location"].startswith(redirect_url)
+
+        with responses.RequestsMock() as rsps:
+            # fake previous account linking
+            rsps.add(responses.GET, "{}/get_id".format(api_url), "test_userid", status=200)
+
+            # incoming account linking response
+            http_resp = test_client.get("/account_linking/handle_account_linking")
+            assert http_resp.status_code == 200
