67e07b55700c91b3cb16f7737d9461664e0af380
Johan Lundberg <lundberg@sunet.se>
2017-05-15 13:02:37 +0200

    Bump version: 3.4.2 → 3.4.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SATOSA',
-    version='3.4.2',
+    version='3.4.3',
     description='Protocol proxy (SAML/OIDC).',
     author='DIRG',
     author_email='satosa-dev@lists.sunet.se',
