4ec911b799ca330f2c3c486b604d90855a78e1e8
Leif Johansson <leifj@sunet.se>
2017-05-23 15:46:09 +0200

    keep track of attribute_name for mustach iterators

--- src/satosa/micro_services/attribute_generation.py
+++ src/satosa/micro_services/attribute_generation.py
@@ -9,8 +9,9 @@ def _config(f, requester, provider):
     return rf.items()
 
 class MustachAttrValue(object):
-    def __init__(self,values):
-       self.values = values
+    def __init__(self, attr_name, values):
+       self._attr_name = attr_name
+       self._values = values
        if any(['@' in v for v in values]):
           local_parts = []
           domain_parts = []
@@ -26,19 +27,23 @@ class MustachAttrValue(object):
           self._scopes = None
 
     def __str__(self):
-        return ";".join(self.values)
+        return ";".join(self._values)
+
+    @property
+    def values(self):
+        [{self._attr_name: v} for v in self._values]
    
     @property 
     def value(self):
-        if 1 == len(self.values): 
-           return self.values[0]
+        if 1 == len(self._values):
+           return self._values[0]
         else:
-           return self.values
+           return self._values
 
     @property
     def first(self):
-        if len(self.values) > 0:
-           return self.values[0]
+        if len(self._values) > 0:
+           return self._values[0]
         else:
            return ""
 
@@ -63,13 +68,13 @@ class AddSyntheticAttributes(ResponseMicroService):
         context = dict()
         
         for attr_name,values in attributes.items():
-           context[attr_name] = MustachAttrValue(values)
+           context[attr_name] = MustachAttrValue(attr_name, values)
 
         recipes = _config(self.synthetic_attributes, requester, provider)
         print(context)
         for attr_name, fmt in recipes:
            print(fmt)
-           syn_attributes[attr_name] = re.split("[;\n]+", pystache.render(fmt, context))
+           syn_attributes[attr_name] = [v.strip().strip(';') for v in re.split("[;\n]+", pystache.render(fmt, context))]
         print(syn_attributes)
         return syn_attributes
 
