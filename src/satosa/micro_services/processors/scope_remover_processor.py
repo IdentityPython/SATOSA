3b324b9e6e48a1b085aa1436fd17b28d5b66b230
Ioannis Kakavas <ikakavas@noc.grnet.gr>
2017-08-23 16:06:47 +0300

    Changes according to @c00kiemon5ter comments

--- src/satosa/micro_services/processors/scope_remover_processor.py
+++ src/satosa/micro_services/processors/scope_remover_processor.py
@@ -1,15 +1,19 @@
-from ..attribute_processor import AttributeProcessorError
+from ..attribute_processor import AttributeProcessorError, AttributeProcessorWarning
 from .base_processor import BaseProcessor
 
 class ScopeRemoverProcessor(BaseProcessor):
     """
-    Removes the scope from a given attribute 
+    Removes the scope from all values of a given attribute 
     
     """
     def process(self, internal_data, attribute, **kwargs):
         attributes = internal_data.attributes
         new_values = []
-        for value in attributes.get(attribute, [None]):
-            if '@' in value:
-                new_values.append(value.split('@')[0])
-        attributes[attribute] = new_values
+        values = attributes.get(attribute, [None])
+        if values:
+            for value in attributes.get(attribute, [None]):
+                unscoped_value = value.split('@')[0]
+                new_values.append(unscoped_value)
+            attributes[attribute] = new_values
+        else:
+            raise AttributeProcessorWarning("Attribute {} is not available".format(attribute))
