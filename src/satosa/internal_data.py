570e08a2b187b946d9d601da8949da8bd7ea1c60
Scott Koranda <skoranda@gmail.com>
2018-06-13 08:05:31 -0500

    Support SAML NameID emailAddress and unspecified in response
    Add logic that allows support for SAML NameID of types
    urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress
    urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified
    urn:oasis:names:tc:SAML:2.0:nameid-format:unspecified
    to be sent in the authentication response sent to SAML SPs.
    
    This commit does not add any logic to help set the value for the
    NameID but if it is set, say by a microservice, and if the metadata
    for the SP specifies one of those types or one of those types is
    specified by the SP in the authentication request, then this commit
    allows the value to make it through and be set in the response.

--- src/satosa/internal_data.py
+++ src/satosa/internal_data.py
@@ -6,7 +6,6 @@ import datetime
 import hashlib
 from enum import Enum
 
-
 class UserIdHashType(Enum):
     """
     All different user id hash types
@@ -15,6 +14,8 @@ class UserIdHashType(Enum):
     persistent = 2
     pairwise = 3
     public = 4
+    emailaddress = 5
+    unspecified = 6
 
     @classmethod
     def from_string(cls, str):
@@ -88,6 +89,9 @@ class UserIdHasher(object):
             user_id = "{req}{id}".format(req=requester, id=user_id)
         elif hash_type == UserIdHashType.public:
             user_id = "{id}".format(id=user_id)
+        elif hash_type == UserIdHashType.emailaddress or \
+             hash_type == UserIdHashType.unspecified:
+            return user_id
         else:
             raise ValueError("Unknown hash type: '{}'".format(hash_type))
 
