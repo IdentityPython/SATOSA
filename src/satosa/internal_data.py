6e074b87a678e1a245e10544ddceaf7acff4cbde
Ivan Kanakarakis <ivan.kanak@gmail.com>
2019-10-08 23:49:19 +0300

    Restore satosa.internal_data compatibility
    Signed-off-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- src/satosa/internal_data.py
+++ src/satosa/internal_data.py
@@ -1,5 +1,12 @@
 import warnings as _warnings
 
+from satosa.internal import InternalData
+from satosa.internal import AuthenticationInformation
+from satosa.deprecated import UserIdHashType
+from satosa.deprecated import UserIdHasher
+from satosa.deprecated import InternalRequest
+from satosa.deprecated import InternalResponse
+
 
 _warnings.warn(
     "internal_data is deprecated; use satosa.internal instead.",
