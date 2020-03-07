e29c6e9434ceaa53e121ab082a8dca0eccbb444d
Ivan Kanakarakis <ivan.kanak@gmail.com>
2020-03-07 22:58:00 +0200

    Remove debug middleware
    Signed-off-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- src/satosa/wsgi.py
+++ src/satosa/wsgi.py
@@ -1,9 +1,7 @@
 import argparse
-import functools
 import os
 import sys
 
-from werkzeug.debug import DebuggedApplication
 from werkzeug.serving import run_simple
 
 from satosa.proxy_server import make_app
@@ -22,18 +20,12 @@ def main():
     parser.add_argument("--keyfile", type=str)
     parser.add_argument("--certfile", type=str)
     parser.add_argument("--host", type=str)
-    parser.add_argument(
-        "-d", action='store_true', dest="debug", help="enable debug mode."
-    )
     args = parser.parse_args()
 
     if (args.keyfile and not args.certfile) or (args.certfile and not args.keyfile):
         print("Both keyfile and certfile must be specified for HTTPS.")
         sys.exit()
 
-    if args.debug:
-        app.app = functools.partial(app.app, debug=True)
-        app = DebuggedApplication(app)
 
     if (args.keyfile and args.certfile):
         ssl_context = (args.certfile, args.keyfile)
