5fcbc753e54a0029ead24cd6041b490534ade042
Ivan Kanakarakis <ivan.kanak@gmail.com>
2020-03-18 16:11:42 +0200

    Update CI process for docker images and tags
    References:
    https://docs.travis-ci.com/user/build-stages/
    https://docs.travis-ci.com/user/build-stages/share-docker-image/
    https://docs.travis-ci.com/user/conditions-v1
    https://docs.travis-ci.com/user/conditional-builds-stages-jobs/
    
    Signed-off-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- scripts/travis_create_docker_image_branch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-export REPO=satosa/satosa
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -f Dockerfile -t $REPO:$TAG .
-docker push $REPO
