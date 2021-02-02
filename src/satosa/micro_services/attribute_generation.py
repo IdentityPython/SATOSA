04f9f9adae925d050b8ba1fe8f892fce235849ff
Sven Haardiek <sven@haardiek.de>
2021-02-02 10:47:25 +0100

    Fix Attibure Generation Microservice
    * Fix broken indentation in the file. Now is is mostly pep8 compatible.
    * Add missing return in `MustachAttrValue.values`
    * Remove `None` from mustache rendered strings
    
    Co-authored-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- src/satosa/micro_services/attribute_generation.py
+++ src/satosa/micro_services/attribute_generation.py
@@ -4,51 +4,52 @@ import pystache
 from .base import ResponseMicroService
 from ..util import get_dict_defaults
 
+
 class MustachAttrValue(object):
     def __init__(self, attr_name, values):
-       self._attr_name = attr_name
-       self._values = values
-       if any(['@' in v for v in values]):
-          local_parts = []
-          domain_parts = []
-          scopes = dict()
-          for v in values:
-             (local_part, sep, domain_part) = v.partition('@')
-             # probably not needed now...
-             local_parts.append(local_part)
-             domain_parts.append(domain_part)
-             scopes[domain_part] = True
-          self._scopes = list(scopes.keys())
-       else:
-          self._scopes = None
+        self._attr_name = attr_name
+        self._values = values
+        if any(['@' in v for v in values]):
+            local_parts = []
+            domain_parts = []
+            scopes = dict()
+            for v in values:
+                (local_part, sep, domain_part) = v.partition('@')
+                # probably not needed now...
+                local_parts.append(local_part)
+                domain_parts.append(domain_part)
+                scopes[domain_part] = True
+            self._scopes = list(scopes.keys())
+        else:
+            self._scopes = None
 
     def __str__(self):
         return ";".join(self._values)
 
     @property
     def values(self):
-        [{self._attr_name: v} for v in self._values]
-   
-    @property 
+        return [{self._attr_name: v} for v in self._values]
+
+    @property
     def value(self):
         if len(self._values) == 1:
-           return self._values[0]
+            return self._values[0]
         else:
-           return self._values
+            return self._values
 
     @property
     def first(self):
         if len(self._values) > 0:
-           return self._values[0]
+            return self._values[0]
         else:
-           return ""
+            return ""
 
     @property
     def scope(self):
         if self._scopes is not None:
-           return self._scopes[0]
+            return self._scopes[0]
         return ""
-     
+
 
 class AddSyntheticAttributes(ResponseMicroService):
     """
@@ -124,13 +125,18 @@ you don't care which value is used in a template.
     def _synthesize(self, attributes, requester, provider):
         syn_attributes = dict()
         context = dict()
-        
-        for attr_name,values in attributes.items():
-           context[attr_name] = MustachAttrValue(attr_name, values)
+
+        for attr_name, values in attributes.items():
+            context[attr_name] = MustachAttrValue(attr_name, values)
 
         recipes = get_dict_defaults(self.synthetic_attributes, requester, provider)
         for attr_name, fmt in recipes.items():
-           syn_attributes[attr_name] = [v.strip().strip(';') for v in re.split("[;\n]+", pystache.render(fmt, context))]
+            syn_attributes[attr_name] = [
+                value
+                for token in re.split("[;\n]+", pystache.render(fmt, context))
+                for value in [token.strip().strip(';')]
+                if value
+            ]
         return syn_attributes
 
     def process(self, context, data):
