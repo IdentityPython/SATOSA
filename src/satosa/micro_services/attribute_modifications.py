df563efbd7534e95f007efffac338d286f5c2b31
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2023-03-15 13:48:45 +1300

    new: FilterAttributeValues: pass context and target_provider through _apply_requester_filters and _apply_filters
    ... for use by specific filters

--- src/satosa/micro_services/attribute_modifications.py
+++ src/satosa/micro_services/attribute_modifications.py
@@ -30,24 +30,24 @@ class FilterAttributeValues(ResponseMicroService):
     def process(self, context, data):
         # apply default filters
         provider_filters = self.attribute_filters.get("", {})
-        self._apply_requester_filters(data.attributes, provider_filters, data.requester)
+        target_provider = data.auth_info.issuer
+        self._apply_requester_filters(data.attributes, provider_filters, data.requester, context, target_provider)
 
         # apply target provider specific filters
-        target_provider = data.auth_info.issuer
         provider_filters = self.attribute_filters.get(target_provider, {})
-        self._apply_requester_filters(data.attributes, provider_filters, data.requester)
+        self._apply_requester_filters(data.attributes, provider_filters, data.requester, context, target_provider)
         return super().process(context, data)
 
-    def _apply_requester_filters(self, attributes, provider_filters, requester):
+    def _apply_requester_filters(self, attributes, provider_filters, requester, context, target_provider):
         # apply default requester filters
         default_requester_filters = provider_filters.get("", {})
-        self._apply_filters(attributes, default_requester_filters)
+        self._apply_filters(attributes, default_requester_filters, context, target_provider)
 
         # apply requester specific filters
         requester_filters = provider_filters.get(requester, {})
-        self._apply_filters(attributes, requester_filters)
+        self._apply_filters(attributes, requester_filters, context, target_provider)
 
-    def _apply_filters(self, attributes, attribute_filters):
+    def _apply_filters(self, attributes, attribute_filters, context, target_provider):
         for attribute_name, attribute_filters in attribute_filters.items():
             if type(attribute_filters) == str:
                 # convert simple notation to filter list
