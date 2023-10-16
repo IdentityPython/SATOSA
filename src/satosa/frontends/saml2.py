c5f022878011b3ae52af4c8ce1b804ddcdb53613
sebulibah <sebuliba.hannah@live.com>
2023-10-16 09:03:09 +0000

    fix: move logout callback to the end in saml frontend module class constructor

--- src/satosa/frontends/saml2.py
+++ src/satosa/frontends/saml2.py
@@ -68,10 +68,10 @@ class SAMLFrontend(FrontendModule, SAMLBaseModule):
     KEY_ENDPOINTS = 'endpoints'
     KEY_IDP_CONFIG = 'idp_config'
 
-    def __init__(self, auth_req_callback_func, logout_req_callback_func, internal_attributes, config, base_url, name):
+    def __init__(self, auth_req_callback_func, internal_attributes, config, base_url, name, logout_req_callback_func=None):
         self._validate_config(config)
 
-        super().__init__(auth_req_callback_func, logout_req_callback_func, internal_attributes, base_url, name)
+        super().__init__(auth_req_callback_func, internal_attributes, base_url, name, logout_req_callback_func)
         self.config = self.init_config(config)
 
         self.endpoints = config[self.KEY_ENDPOINTS]
@@ -1001,9 +1001,9 @@ class SAMLVirtualCoFrontend(SAMLFrontend):
     KEY_ORGANIZATION = 'organization'
     KEY_ORGANIZATION_KEYS = ['display_name', 'name', 'url']
 
-    def __init__(self, auth_req_callback_func, logout_req_callback, internal_attributes, config, base_url, name):
+    def __init__(self, auth_req_callback_func, internal_attributes, config, base_url, name):
         self.has_multiple_backends = False
-        super().__init__(auth_req_callback_func, logout_req_callback, internal_attributes, config, base_url, name)
+        super().__init__(auth_req_callback_func, internal_attributes, config, base_url, name)
 
     def handle_authn_request(self, context, binding_in):
         """
